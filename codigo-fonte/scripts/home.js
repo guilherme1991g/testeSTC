496139ef765ce2a1a732d394fb1a31f8b7012dc5
Guilherme Silva <73372109+guilherme1991g@users.noreply.github.com>
2024-11-07 15:07:27 -0300

    TESTE

--- codigo-fonte/scripts/home.js
+++ codigo-fonte/scripts/home.js
@@ -1,6 +1,6 @@
 const { request } = require("request");
 
-    const token = "4962a431d48bdc7d85fbe39d430f8421191f6f6c9fe4593529cd9295177dc37c8e7f2a2e7d5bd7ab42258b1752a624c857cfa950a931fc1b822fd6885c847a2265c80bab40556abd4b8c782e08d8450108f990417ef0f723e7c3998def8b61668fdda80c0bd818be85f295a3641d8bf85857f054461c0992d50c8f4dc48eb149cbe5f5690d95d69d9c05fe92864c2ed4"; // API Token
+    const token = "c4f162f7f90c2713ab536ff04a8ffe2edc15019d0ee567a9e720b7b7b30d6fb33b373c89c6ce2df9b3337b41b8c1496bab95ab487ee1b8933365d3a15414501e919c3306ef7e64f83674bd8f6e68b975"; // API Token
     
     var options = {
       'method': 'GET',
@@ -10,6 +10,8 @@ const { request } = require("request");
         "Content-Type": "application/json"	
       },
       formData: {
+        "usuario": "guilherme1991g",
+        "senha": "Gui1991g"
     
       }
     };
--- codigo-fonte/scripts/relatorio-vendas.js
+++ codigo-fonte/scripts/relatorio-vendas.js
@@ -1,22 +1,14 @@
-const url = "http://ap3.stc.srv.br/integration/prod/ws/vehicle/update"; // API URL
-const method = "POST"; // Request method, change for what's needed
+const url = "https://api.hinova.com.br/api/sga/v2/buscar/situacao-veiculo/PFC7899"; // API URL
+const method = "GET"; // Request method, change for what's needed
 
-const update = {
-    key: 'd4cc8469585856eb0e1161559ebb0dcd',
-    id: "511538563",
-    manufacture: "st",
-    lisencePlate: "chs0053",
-    status: "1",
-
-};
 
 fetch(url, {
     method,
     headers: {
+        "Authorization": `Bearer 0faeb64da48fad399ca9f1648490b9d2ba3fb3ba1d56eeddd724c0d0a7b10e36a377bf706ee0a20d69e45a0dadc5a192c1b2070b0b926fd08fd02b303602f82271442f36a200670ae3f45c9eb14f6bcf4ff86a443e340d638d92408c4574d2b7cd9b6851fb8b638eabbb2f432519df2342af06319786363acf6373c7a65a281176be2142b7c219c524be537f019b6155`,
         "Content-Type": "application/json"	 // This is the important part, the auth header
     },
 
-    body: JSON.stringify(update),
 
 }).then((response) => {
     if (!response.ok) {
@@ -26,11 +18,10 @@ fetch(url, {
 })
     .then((data) => {
 
-        for (i=5 ; i>0 ; i--){
 
         var placa = data
         console.log(placa)
-        }
+
     })
     .catch((error) => console.error("Fetch error:", error));
 
